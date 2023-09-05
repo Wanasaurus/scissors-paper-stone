3605a46b52135d68488dc4ab438ffdbea6bdaadb
Wanasaurus <wanasaurus@hotmail.com>
2023-09-05 18:15:47 +0800

    Add an alert message for when player exits game

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 let playerSelection = prompt("Let's play scissors paper stone! Please enter one of three options: scissors, paper, stone");
+if (playerSelection == null || playerSelection == "") {
+    alert("That's OK, come play another day.")
+}
