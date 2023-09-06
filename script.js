5261837767ee560c57c41d2bcd612d1a99e5803a
Wanasaurus <wanasaurus@hotmail.com>
2023-09-06 14:59:45 +0800

    Create getComputerChoice() function

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let playerSelection = prompt("Let's play scissors paper stone! Please enter one of three options: scissors, paper, stone");
+
 if (playerSelection == null || playerSelection == "") {
     alert("That's OK, come play another day.")
+}
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return "Scissors";
+    } else if (choice === 1) {
+        return "Paper";
+    } else if (choice === 2) {
+        return "Stone";
+    }
 }
