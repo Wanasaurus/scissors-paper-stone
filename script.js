ef69e0260fa18f56081720a4c364b2a7ae54ba5e
Wanasaurus <wanasaurus@hotmail.com>
2023-09-16 19:48:49 +0800

    Write code for fourth and fifth rounds

--- script.js
+++ script.js
@@ -60,3 +60,11 @@ console.log(playRound(playerSelect, computerSelect));
 playerSelect = getPlayerChoice();
 computerSelect = getComputerChoice();
 console.log(playRound(playerSelect, computerSelect));
+
+playerSelect = getPlayerChoice();
+computerSelect = getComputerChoice();
+console.log(playRound(playerSelect, computerSelect));
+
+playerSelect = getPlayerChoice();
+computerSelect = getComputerChoice();
+console.log(playRound(playerSelect, computerSelect));
